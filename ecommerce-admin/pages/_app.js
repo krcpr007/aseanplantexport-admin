956cbc16e62bd791750dbe9c81409fe142655543
Rajan kumar <krcpr007@gmail.com>
2023-10-07 11:22:55 +0530

    Title added

--- ecommerce-admin/pages/_app.js
+++ ecommerce-admin/pages/_app.js
@@ -1,11 +1,19 @@
 import '@/styles/globals.css';
 import 'styles/stats.css';
 import { SessionProvider } from "next-auth/react";
-
-export default function App({Component, pageProps: { session, ...pageProps }}) {
+import Head from 'next/head';
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps}/>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Asean Plant Export | Thailand Supplier of Thai Plants | Rhizome, Tubers, Bulbs, Seeds, Hoy</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Asean Plant Export is one of the World class supplier from Thailand for Seeds, Rhizome, Tubers, Bulbs and Musa world wide" />
+        <meta name="keywords" content="Caudex Plants, Flowering Plants, Shrubs, Fruit Trees, Hoya Plants, Hoya Rooted Cuttings, Mangrove, Musa, Banana, Materials & Decoration, Seeds, Tuber, Bulbs, Rhizomes, Waterlily , Aquatic Plants, Adenium,Plumeria, Lotus Rhizomes, Giant Grafted Hibiscus, Books" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
