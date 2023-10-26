8f373725601cb5b657e4904fcb8d4b2d4dfc7c64
Rajan kumar <krcpr007@gmail.com>
2023-10-26 10:22:30 +0530

    Update : Admin model

--- ecommerce-admin/models/Admin.js
+++ ecommerce-admin/models/Admin.js
@@ -2,6 +2,7 @@ import {model, models, Schema} from "mongoose";
 
 const adminSchema = new Schema({
   email: {type: String, required: true, unique: true},
+  isSuperAdmin: {type: Boolean, default: false}, // such as root user will be never deleted
 }, {timestamps: true});
 
 export const Admin = models?.Admin || model('Admin', adminSchema);
