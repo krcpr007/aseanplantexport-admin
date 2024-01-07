6e9701e12f8afa935d247f4eb50c33022b8309db
Rajan kumar <krcpr007@gmail.com>
2024-01-07 19:54:28 +0530

    custom-color

--- ecommerce-front/tailwind.config.js
+++ ecommerce-front/tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    "./landingPageElement/**/*.{js,ts,jsx,tsx,mdx}",
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-green': '#9DE62A',
+        'light-black': '#1B1B1B'
+      }
+    },
   },
   plugins: [],
 }
