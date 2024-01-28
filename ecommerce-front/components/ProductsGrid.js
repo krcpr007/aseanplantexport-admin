1781df66fab12bc1c1847af397e5c6a501a38218
Rajan kumar <krcpr007@gmail.com>
2024-01-28 17:59:40 +0530

    Update: now title wil be a props

--- ecommerce-front/components/ProductsGrid.js
+++ ecommerce-front/components/ProductsGrid.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import ProductBox from "@/components/ProductBox";
 import { RevealWrapper } from 'next-reveal'
+import Title from "./Title";
 
 const StyledProductsGrid = styled.div`
   display: grid;
@@ -11,16 +12,19 @@ const StyledProductsGrid = styled.div`
   }
 `;
 
-export default function ProductsGrid({ products, wishedProducts = [] }) {
-  return (<>
+export default function ProductsGrid({ title, products, wishedProducts = [] }) {
+  return (
+  <>
     {/* <StyledProductsGrid interval={100}> */}
     {/* <div className="grid grid-cols-3 gap-3"> */}
     <div className="min-h-screen bg-gradient-to-tr from-green-300 to-white flex justify-center items-center py-20">
-      <div className="md:px-4 grid sm:grid-cols-2 lg:grid-cols-4 gap-5 space-y-4 md:space-y-0">
+     <div>
+     <Title><span  dangerouslySetInnerHTML={{ __html: title}}/></Title>
+      <div className="md:px-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 space-y-4 md:space-y-0">
         {products?.length > 0 && products.map((product, index) => (
-          // <RevealWrapper key={product._id} delay={index * 50}>
-          <ProductBox {...product} wished={wishedProducts.includes(product._id)}  key={product._id}/>
-          // </RevealWrapper>
+          <RevealWrapper key={product._id} delay={index*10}>
+            <ProductBox {...product} wished={wishedProducts.includes(product._id)}  key={product._id}/>
+          </RevealWrapper>
 
           // <div className="min-h-screen bg-gray-900 flex items-center">
 
@@ -29,6 +33,7 @@ export default function ProductsGrid({ products, wishedProducts = [] }) {
         {/* </StyledProductsGrid> */}
         {/* </div> */}
       </div>
+     </div>
     </div >
   </>
   );
