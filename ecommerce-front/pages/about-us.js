48ad4206aca48b904d2cf8ed8008b9f572d1c857
Rajan kumar <krcpr007@gmail.com>
2024-02-11 23:03:53 +0530

    update : two pages added

---
+++ ecommerce-front/pages/about-us.js
@@ -0,0 +1,12 @@
+import Header from '@/landingPageElement/Header'
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <>
+    <Header/>
+    about-us</>
+  )
+}
+
+export default AboutUs
---
+++ ecommerce-front/pages/contact-us.js
@@ -0,0 +1,12 @@
+import Header from '@/landingPageElement/Header'
+import React from 'react'
+
+function ContactUS() {
+  return (
+    <>
+    <Header/>
+    ContactUS</>
+  )
+}
+
+export default ContactUS
