78aa4dc075de1b15abc218a1a3f7380a71c0d9cf
Rajan kumar <krcpr007@gmail.com>
2024-02-13 15:50:21 +0530

    code refactor: breadcrumb added and fixed class issue

--- ecommerce-front/pages/search.js
+++ ecommerce-front/pages/search.js
@@ -47,12 +47,12 @@ export default function SearchPage() {
       <Header />
       <Center>
         {/* <InputWrapper> */}
-        <div class="max-w-md mx-auto my-10">
-          <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-            <div class="grid place-items-center h-full w-12 text-gray-300">
+        <div className="max-w-md mx-auto my-10">
+          <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+            <div className="grid place-items-center h-full w-12 text-gray-300">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
+                className="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -67,7 +67,7 @@ export default function SearchPage() {
             </div>
 
             <input
-              class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+              className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
               type="text"
               id="search"
               placeholder="Search on Asean plant export.."
@@ -86,7 +86,9 @@ export default function SearchPage() {
       </div>
       {isLoading && <Spinner fullWidth={true} />}
       {!isLoading && products.length > 0 && (
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} pages={[
+          { name: "Search", href: "/search", current: false },
+        ]} />
       )}
     </section>
   );
