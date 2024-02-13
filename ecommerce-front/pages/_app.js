31f3fc6e2851c1d6d07714a9155415256a27284a
Rajan kumar <krcpr007@gmail.com>
2024-02-13 15:41:39 +0530

    layout added

--- ecommerce-front/pages/_app.js
+++ ecommerce-front/pages/_app.js
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 import { CartContextProvider } from "@/components/CartContext";
 import { SessionProvider } from "next-auth/react";
-import Head from "next/head";
-import '../styles/globals.css'
+import Layout from "@/layout/Layout";
+
+import "../styles/globals.css";
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
   body{
@@ -18,22 +19,20 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <>
-      <Head>
-        <title>Asean Plant Export | Thailand Supplier of Thai Plants | Rhizome, Tubers, Bulbs, Seeds, Hoy</title>
-        <link rel="icon" href="/Cacti.webp" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Asean Plant Export is one of the World class supplier from Thailand for Seeds, Rhizome, Tubers, Bulbs and Musa world wide" />
-        <meta name="keywords" content="Caudex Plants, Flowering Plants, Shrubs, Fruit Trees, Hoya Plants, Hoya Rooted Cuttings, Mangrove, Musa, Banana, Materials & Decoration, Seeds, Tuber, Bulbs, Rhizomes, Waterlily , Aquatic Plants, Adenium,Plumeria, Lotus Rhizomes, Giant Grafted Hibiscus, Books" />
-      </Head>
-      <GlobalStyles />
-      <SessionProvider session={session}>
-        <CartContextProvider>
-          <Component {...pageProps} />
-        </CartContextProvider>
-      </SessionProvider>
+      <Layout>
+        <GlobalStyles />
+        <SessionProvider session={session}>
+          <CartContextProvider>
+            <Component {...pageProps} />
+          </CartContextProvider>
+        </SessionProvider>
+      </Layout>
     </>
   );
 }
