6514fb707200604ea7de1fddaea6cfce924247a9
Rajan kumar <krcpr007@gmail.com>
2024-02-13 15:42:46 +0530

    Additional page created

--- ecommerce-front/pages/contact-us.js
+++ ecommerce-front/pages/contact-us.js
@@ -1,11 +1,67 @@
 import Header from '@/landingPageElement/Header'
 import React from 'react'
-
+import Footer from '@/landingPageElement/Footer'
 function ContactUS() {
   return (
     <>
     <Header/>
-    ContactUS</>
+    <div className="contact-1 py-4 md:py-12">
+  <div className="container mx-auto px-4">
+    <div className="xl:flex -mx-4">
+      <div className="xl:w-10/12 xl:mx-auto px-4">
+
+        <div className="xl:w-3/4 mb-4">
+          <h1 className="text-3xl text-medium mb-4">We would love to hear from you</h1>
+          <p className="text-xl mb-2">Please submit your information and we will get back to you.</p>
+          <p>Call us at <a href="tel:+12314561231" className="text-green-600 border-b border-transparent hover:border-green-600 transition-colors duration-300">+1 231 456 1231</a></p>
+        </div>
+
+        <div className="md:flex md:-mx-4 mt-4 md:mt-10">
+
+          <div className="md:w-2/3 md:px-4">
+            <div className="contact-form">
+              <div className="sm:flex sm:flex-wrap -mx-3">
+                <div className="sm:w-1/2 px-3 mb-6">
+                  <input type="text" placeholder="Full Name" className="border-2 rounded px-3 py-1 w-full focus:border-green-400 input"/>
+                </div>
+                <div className="sm:w-1/2 px-3 mb-6">
+                  <input type="text" placeholder="Company Name" className="border-2 rounded px-3 py-1 w-full focus:border-green-400 input"/>
+                </div>
+                <div className="sm:w-1/2 px-3 mb-6">
+                  <input type="text" placeholder="E-mail address" className="border-2 rounded px-3 py-1 w-full focus:border-green-400 input"/>
+                </div>
+                <div className="sm:w-1/2 px-3 mb-6">
+                  <input type="text" placeholder="Phone Number" className="border-2 rounded px-3 py-1 w-full focus:border-green-400 input"/>
+                </div>
+                <div className="sm:w-full px-3">
+                  <textarea name="message" id="message" cols="30" rows="4" placeholder="Your message here" className="border-2 rounded px-3 py-1 w-full focus:border-green-400 input"></textarea>
+                </div>
+              </div>
+              <div className="text-right mt-4 md:mt-12">
+                <button className="border-2 border-green-600 rounded px-6 py-2 text-green-600 hover:bg-green-600 hover:text-white transition-colors duration-300">
+                  Send a Message
+                  <i className="fas fa-chevron-right ml-2 text-sm"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="md:w-1/3 md:px-4 mt-10 md:mt-0">
+            <div className="bg-green-100 rounded py-4 px-6">
+              <h5 className="text-xl font-medium mb-3">Help</h5>
+              <p className="text-gray-700 mb-4">Need help or have any query? Don't hesitate, you can directly shoot us an <a href="mailto:" className="text-green-600 border-b border-transparent hover:border-green-600 inline-block">email</a> or call us at <a href="tel:" className="text-green-600 border-b border-transparent hover:border-green-600 inline-block">+1 231 456 1231</a></p>
+              <p className="text-gray-700">You can move to <a href="#" className="text-green-600 border-b border-transparent hover:border-green-600 inline-block">FAQs</a> or <a href="#" className="text-green-600 border-b border-transparent hover:border-green-600 inline-block">Support</a> page to get more information about our site.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+</>
   )
 }
 
---
+++ ecommerce-front/pages/privacy-policy.js
@@ -0,0 +1,79 @@
+import Header from "@/landingPageElement/Header";
+import React from "react";
+
+function PrivacyPolicy() {
+  return (
+    <>
+    <Header/>
+    <div className="px-5 md:px-20">
+      <div className="pt-5">
+        <h1 className="text-center text-3xl font-semibold text-black">Privacy Policy</h1>
+      </div>
+      <div className="my-5">
+        <h2 className="text-2xl underline text-black">DISCLAIMER</h2>
+        <p className="text-black text-justify">By ordering this product you agree to the following terms and conditions <br/>
+Plant delivery from Thailand <br/>
+Please note that this plant will be delivered from Thailand.<br/> Most countries have custom rules on letting the plants in. Those rules are aimed to protect the wild flora of the export country (Thailand) as well as not letting plant diseases into the import country (your country). We can absolutely assure that all shipped plants are healthy and nursery grown only (no wild collected plants) and are shipped legally from Thailand.<br/>
+We ship all packages marked as “highly perishable plants / Handle it with outmost care”. All legal documents as Phytosanitary papers in triple and invoice will be attached by us in a plastic pouch on the outside of the box.<br/>
+If we ship by private courier than the documents will be removed by our agent on entrée in Miami / Vancouver / London when they clear the box for your zone.<br/>
+Reason why this is removed is simple. If we keep the docs on the box then the package will go again through local customs and you will be charged vat, taxes, handling. If we remove the documents on entrée, then the package will fly direct to your door with no extra costs.<br/>
+U agreed on this sale, and this also means u accept the risk, you know the risk and u accept no refund can be done. By ordering you acknowledge and accept the risk of the package to be destroyed by the custom services of your country as well as possible inappropriate handling of the package by your mail service. Each order will receive the tracking number.</p>
+      </div>
+      <div className="my-5">
+        <h2 className="text-2xl underline text-black">International regulations about shipping perishables</h2>
+        <p className="text-black  text-justify"> The revised and update rules and regulations on international shipment effective 1 January 2020 The International Chamber of Commerce under the Perishable Cargo Goods, certain items are exempted from being refunded, returned and / or exchanged such is, cut flowers, certain foods, and live plants. Live plants are indeed categorized as perishable goods.
+According to international treaties, neither country nor business entity can enforce laws that supersede international law. Domestic laws must be under Country law, which must be under International law.
+If there is any doubt about the item you sent, you simply scan in a copy of the Phyto which is you prove that the plant was alive, healthy and without defect at the moment of dispatch.
+With airfreight FCA applies and for this incoterm the risk shifts to the buyer the moment the cargo is collected from your door. You have no obligation to refund, not even for damages incurred by couriers and carriers.</p>
+      </div>
+      <div className="my-5">
+        <h2 className="text-2xl underline text-black">Shipping cost</h2>
+        <p className="text-black text-justify">When you add the desired item to your shopping cart, you will see there the shipping cost based on the destination country and estimated item weight (or total weight of the items). For practical reasons we maintain only two shipping rates based on the destination: for orders to USA and for orders to other shipped countries. The actual shipping cost paid by the seller when sending your package to you may differ from the cost your paid for the shipment (it can be more or less). This is due to the differences in actual shipping costs to each individual country and the differences in weight of each plant. However the shipping cost applied to your order is final, not negotiable and not refundable</p>
+      </div>
+      <div className="my-5">
+        <h2 className="text-2xl underline text-black">Shipping During winter time</h2>
+        <p className="text-black">Certain parts of the world have cold winters and that can affect the plants of course. Heat packs are useless since they only work 72h max and zero packages arrive within 3 days. We can and we do pack of course better for winter time but when your postal service stores the package 2-3 days in sub-zero temperatures then nothing survives.
+So please consider before you buy, I think u are better placed to know if the temperature is acceptable or not. We do not take responsibility, neither refund when damage to the plants are done by your local post bad storage.</p>
+      </div>
+      <div className="my-5">
+        <h2 className="text-2xl underline text-black">Message for all resell-online stores.</h2>
+        <p className="font-medium text-black">Because we get this question at least 5 times a week “we cannot sell direct”.
+We have hundreds of wholesale customers around the world and 100% know that ALL plants need minimum 6 weeks recovery before they can be sold.
+But I see in almost all smaller online-stores that they expect a “arrive-unpack-sell-ship out tomorrow” situation.
+That’s impossible because removing soil, shipping plants in a dark box, changing temps, rough handling…is already a murder on any plant.
+Keep in mind that removing soil basically destroys 90% of all active roots.
+People who fly 12h relaxed on an airplane already complain that this is a nightmare, and they need few days “recovery” before they can go back to normal.
+Plants 2 week in a dark box should be no problem?
+These plants come from a nice tropical region with good humid air and in most cases, they arrive at room temperature, dry air and hopefully some sun.
+They are exhausted, they need care like a baby for six week and then u can evaluate your plant and yes even then not 100% will survive, why?
+Very simple, not all plants can take this horrible shipment.
+
+</p>
+      </div>
+      <div>
+        <h2 className="text-2xl underline text-black">
+        Full documentation from the seller, including:
+        </h2>
+        <div>
+            <ul className="list-disc list-inside text-black">
+                <li> export license</li>
+                <li> EUN license</li>
+                <li> EUE license</li>
+                <li> Full phytosanitary docs</li>
+                <li> controlled plant export license</li>
+                <li>  controlled nursery license</li>
+                <li> treatment license and</li>
+                <li> additional attachment</li>
+                <li>  foreign trade license</li>
+                <li> Gekko Enterprise Co., Ltd. &gt; PNG50 / PP20 / BS58</li>
+                <li> packing list, invoice and application</li>
+                <li> Included all transport and working hours (4h per shipment)up and down between our offices and trade department</li>
+            </ul>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default PrivacyPolicy;
