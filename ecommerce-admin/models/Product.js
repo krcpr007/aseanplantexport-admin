9eed54ed3ad07bab6af4abffdf3b00ee30720096
Rajan kumar <krcpr007@gmail.com>
2024-02-13 15:43:52 +0530

    Feat : product weight added in schema

--- ecommerce-admin/models/Product.js
+++ ecommerce-admin/models/Product.js
@@ -3,6 +3,7 @@ import mongoose, {model, Schema, models} from "mongoose";
 const ProductSchema = new Schema({
   title: {type:String, required:true},
   description: String,
+  weight: Number,
   price: {type: Number, required: true},
   images: [{type:String}],
   category: {type:mongoose.Types.ObjectId, ref:'Category'},
