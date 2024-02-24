b5a056a93375852e2b5324fd1355715a9a66c738
Rajan kumar <krcpr007@gmail.com>
2024-02-24 13:07:24 +0530

    Fix : warning removed

--- ecommerce-front/pages/404.js
+++ ecommerce-front/pages/404.js
@@ -1,36 +1,36 @@
 import Header from "@/landingPageElement/Header";
-import React from "react";
+import Link from 'next/link'
 
 function Error() {
   return (
     <>
       <Header />
       <div>
-        <div class="flex flex-col bg-gray-50 items-center justify-center h-screen">
-          <h1 class="text-4xl font-bold mb-4">404 - Not Found</h1>
-          <p class="text-gray-600">
+        <div className="flex flex-col bg-gray-100 items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+          <p className="text-gray-600">
             Sorry, the page you're looking for doesn't exist.
           </p>
 
-          {/* <div class="mt-6">
+          {/* <div className="mt-6">
             <input
               type="text"
               placeholder="Search for content..."
-              class="px-4 py-2 border rounded-lg focus:outline-none focus:border-green-500"
+              className="px-4 py-2 border rounded-lg focus:outline-none focus:border-green-500"
             />
           </div> */}
 
-          <div class="mt-4 text-center">
-            <p class="text-gray-600 mb-3">Or, you might want to explore:</p>
-            <a href="/category" class="text-green-500 hover:underline">
+          <div className="mt-4 text-center">
+            <p className="text-gray-600 mb-3 font-medium">Or, you might want to explore:</p>
+            <Link href="/category" className="text-green-500 ">
               Categories
-            </a>
-            <a href="/products" class="text-green-500 hover:underline ml-2">
+            </Link>
+            <Link href="/products" className="text-green-500  ml-2">
               Products
-            </a>
-            <a href="/contact-us" class="text-green-500 hover:underline ml-2">
+            </Link>
+            <Link href="/contact-us" className="text-green-500  ml-2">
               Contact
-            </a>
+            </Link>
           </div>
         </div>
       </div>
